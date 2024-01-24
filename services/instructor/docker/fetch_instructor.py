601748f83801124be2282db4a1e086b167a331ad
Kord Campbell <kordless@gmail.com>
2024-01-24 13:48:08 -0600

    instructor container

--- services/instructor/docker/fetch_instructor.py
+++ services/instructor/docker/fetch_instructor.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
 	print("Fetching instructor data...")
 	xl = INSTRUCTOR('hkunlp/instructor-xl')
 	large = INSTRUCTOR('hkunlp/instructor-large')
+	embeddings = xl.encode("install").tolist()
+    embeddings = large.encode("install").tolist()
 	print("Instructor data fetched successfully.")
