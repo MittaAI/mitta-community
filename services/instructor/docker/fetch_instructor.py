7246a22726eb03ad34d00f0561e6220b641eac10
Kord Campbell <kordless@gmail.com>
2024-01-24 20:57:15 -0600

    update cookbooks with new config

--- services/instructor/docker/fetch_instructor.py
+++
@@ -1,9 +0,0 @@
-from InstructorEmbedding import INSTRUCTOR
-
-if __name__ == "__main__":
-	print("Fetching instructor data...")
-	xl = INSTRUCTOR('hkunlp/instructor-xl')
-	large = INSTRUCTOR('hkunlp/instructor-large')
-	embeddings = xl.encode("install").tolist()
-	embeddings = large.encode("install").tolist()
-	print("Instructor data fetched successfully.")
