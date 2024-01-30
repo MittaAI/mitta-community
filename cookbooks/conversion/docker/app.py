a55b9548dda0ccfc5035a87eccaaf9b5dba1aa71
Kord Campbell <kordless@gmail.com>
2024-01-30 12:25:32 -0600

    logailytics.pizza

--- cookbooks/conversion/docker/app.py
+++ cookbooks/conversion/docker/app.py
@@ -10,7 +10,7 @@ from quart_cors import cors
 import httpx
 
 app = Quart(__name__, static_folder='static')
-app = cors(app, allow_origin=["http://localhost:5000", "https://ai.mitta.ai"])
+app = cors(app, allow_origin=["https://logailytics.pizza", "https://ai.mitta.ai"])
 
 # Configure basic logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
