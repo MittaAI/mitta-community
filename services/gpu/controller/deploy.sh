d958c7e97675e58e284ca6c78c83dec8cf4eb31f
Kord Campbell <kordless@gmail.com>
2024-02-12 20:34:05 -0600

    instructor/pdf

--- services/gpu/controller/deploy.sh
+++ services/gpu/controller/deploy.sh
@@ -174,7 +174,7 @@ gcloud compute instances create $NAME-$NEW_UUID \
 $PREEMPTIBLE \
 --service-account=$SERVICE_ACCOUNT \
 --scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
---create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,mode=rw,size=100,type=projects/$GC_PROJECT/zones/$ZONE/diskTypes/pd-ssd \
+--create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231113,mode=rw,size=100,type=projects/$GC_PROJECT/zones/$ZONE/diskTypes/pd-ssd \
 --no-shielded-secure-boot \
 --shielded-vtpm \
 --shielded-integrity-monitoring \
