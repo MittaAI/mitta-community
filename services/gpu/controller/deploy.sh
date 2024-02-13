328eddb16af4e89522054e5ca4f636d904340b30
Kord Campbell <kordless@gmail.com>
2024-02-12 19:24:17 -0600

    instructor/pdf

--- services/gpu/controller/deploy.sh
+++ services/gpu/controller/deploy.sh
@@ -78,11 +78,10 @@ remote_url=$(git remote get-url origin)
 if [[ "$remote_url" =~ ^git@github.com:(.+)/(.+).git$ ]]; then
     user="${BASH_REMATCH[1]}"
     repo="${BASH_REMATCH[2]}"
-    https_url="https://github.com/${user}/${repo}.git"
-    echo $https_url
+    REPO_URL="https://github.com/${user}/${repo}.git"
 # Else, assume it's already an HTTPS URL, just remove the authentication part if present
 else
-    https_url=$(echo $remote_url | sed -E 's/https:\/\/[^@]+@/https:\/\//')
+    REPO_URL=$(echo $remote_url | sed -E 's/https:\/\/[^@]+@/https:\/\//')
     echo $https_url
 fi
 
