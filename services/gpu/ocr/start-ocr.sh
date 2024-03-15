63b935d5c1b059a4a540948ffb3b2c0a804c87a0
Kord Campbell <kordless@gmail.com>
2024-03-15 16:27:53 -0500

    requirements

--- services/gpu/ocr/start-ocr.sh
+++ services/gpu/ocr/start-ocr.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 while true; do
-    screen -dmS gunicorn_screen gunicorn -b 0.0.0.0:8989 app:app -w 8 --timeout 120
+    screen -dmS hypercorn_screen hypercorn -b 0.0.0.0:8989 app:app -w 8 --timeout 120
     sleep 1
     
-    while screen -list | grep -q gunicorn_screen; do
+    while screen -list | grep -q hypercorn_screen; do
         sleep 10
     done
     
-    echo "Gunicorn exited. Restarting..."
+    echo "Hypercorn exited. Restarting..."
     sleep 5
 done
 
