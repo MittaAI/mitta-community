7b829de74dc6ba8846c34870e15b66ebb96a9dbf
Kord Campbell <kordless@gmail.com>
2024-05-07 12:22:25 -0500

    scale factor

--- services/playwright/docker/grub2.py
+++ services/playwright/docker/grub2.py
@@ -151,7 +151,7 @@ async def take_screenshot_and_extract_links(url: str, filename: str = "example.p
             }}''')
 
         # Take a screenshot after increasing the font size
-        await page.screenshot(path=filename, full_page=full_screen)
+        await page.screenshot(path=filename, full_page=full_screen, device_scale_factor=2)
 
         image_from_page = None
         if extract_image:
